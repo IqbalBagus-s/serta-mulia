f0a4e7b5922363f09cba3ca0a685c156f020d6aa
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2024-11-27 23:24:12 +0700

    update storedata

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,13 +1,9 @@
 const { Firestore } = require('@google-cloud/firestore');
- 
+
 async function storeData(id, data) {
-  const db = new Firestore({
-    projectId: 'eastern-period-442803-d2', // pastikan project ID benar
-    keyFilename: '/mnt/d/Downloads/eastern-period-442803-d2-9eade08bc3cd.json'    // path ke file kredensial
-  });
- 
+  const db = new Firestore(); // Konfigurasi otomatis
   const predictCollection = db.collection('prediction');
   return predictCollection.doc(id).set(data);
 }
- 
+
 module.exports = storeData;
